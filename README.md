e94c16e17bb34f33290e70842f30ff18bfbd6c8c
Andrei Shevel <andrei.shevel@indy.app>
2022-06-12 19:25:46 +0200

    Initial library version

---
+++ README.md
@@ -0,0 +1,3 @@
+# bear-bus
+
+Dead simple library for handling events at web application.
---
+++ index.js
@@ -0,0 +1,24 @@
+class Events {
+  events = {};
+
+  emit(eventName, ...args) {
+    this.events[eventName]?.forEach((callback) => callback(...args));
+  }
+
+  on(eventName, callback) {
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(callback);
+  }
+
+  off(eventName, callback) {
+    if (!callback) {
+      delete this.events[eventName];
+    } else if (this.events[eventName]) {
+      this.events[eventName] = this.events[eventName].filter(
+        (fn) => fn !== callback
+      );
+    }
+  }
+}
+
+export default Events;
